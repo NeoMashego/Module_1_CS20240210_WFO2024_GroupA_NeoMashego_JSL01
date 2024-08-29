fb7d50caac50b82b026c735317fde06d15666db7
NeoMashego <mashegonoj@gmail.com>
2024-08-29 13:09:07 +0200

    let parameters created, if statement with parameters added with result content

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 function validateSyntax() {
+    //set parameters
     let input = document.getElementById('petInput').value;
+    let num = /^\d{4}$/g;
+    let char = /[^a-zA-Z]/g;
     // Validation logic goes here
-    let result = ''; // Placeholder for validation result
+    let result = document.getElementById('result'); // Placeholder for validation result
 
     // TODO: Write your validation logic here
         // Check if input starts with 'pet_' and followed by alphanumeric characters
-
-            document.getElementById('result').innerText = result;
-}
+        if (input.startsWith('pet_') && num && char) {
+            result.innerHTML = "Valid Syntax 🟢"
+        }
+         else {result.innerHTML = "Invalid Syntax 🔴"};
+    }
 
 
