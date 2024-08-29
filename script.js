5c77322e1f4e5b325c8c6e48da0c31a63869ff50
NeoMashego <mashegonoj@gmail.com>
2024-08-29 21:06:01 +0200

    parameters changed and function placed in one area, input .match used to validate syntax

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 function validateSyntax() {
     //set parameters
     let input = document.getElementById('petInput').value;
-    let num = /^\d{4}$/g;
-    let char = /[^a-zA-Z]/g;
+    let parameter = /^pet_\d{4}[a-zA-Z]+$/g;
     // Validation logic goes here
     let result = document.getElementById('result'); // Placeholder for validation result
 
     // TODO: Write your validation logic here
         // Check if input starts with 'pet_' and followed by alphanumeric characters
-        if (input.startsWith('pet_') && num && char) {
+        if (input.match(parameter)) {
             result.innerHTML = "Valid Syntax 🟢"
         }
          else {result.innerHTML = "Invalid Syntax 🔴"};
